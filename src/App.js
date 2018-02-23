ba54100fe5db7af633acc510b74236ce45634934
JarekSopinski <jarek.sopinski@gmail.com>
2018-02-23 15:14:24 +0100

    reorganized files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-
-import ContactList from './components/ContactList';
+import AddContactForm from "./components/AddContactForm";
 
 class App extends Component {
-  render() {
-    return (
-        <React.Fragment>
-            <ContactList/>
-        </React.Fragment>
-    );
-  }
+
+    state = {
+        contacts: [
+            {name: 'Luke Skywalker', phone: '111-111-111', email: 'luke@gmail.com', category: 'family'},
+            {name: 'Darth Vader', phone: '222-222-222', email: 'vader@gmail.com', category: 'family'},
+            {name: 'Leia Organa', phone: '333-333-333', email: 'leia@gmail.com', category: 'family'},
+            {name: 'Han Solo', phone: '444-444-444', email: 'han@gmail.com', category: 'job'},
+            {name: 'Lando Carlissian', phone: '555-555-555', email: 'lando@gmail.com', category: 'job'},
+            {name: 'Obi-wan Kenobi', phone: '777-777-777', email: 'kenobi@gmail.com', category: 'job'}
+        ]
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Contact list</h1>
+                <AddContactForm/>
+            </React.Fragment>
+        )
+    };
+
 }
 
 export default App;
---
+++ src/components/AddContactForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AddContactForm extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <form>
+                    <input/>
+                    <button>Add new contact</button>
+                </form>
+            </React.Fragment>
+        )
+    }
+
+}
+
+export default AddContactForm
